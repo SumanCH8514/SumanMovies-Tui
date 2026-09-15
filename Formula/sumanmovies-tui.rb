38709e240e8d0265d3c42a6ed89a1bad49c3ace0
SumanCH8514 <84062827+SumanCH8514@users.noreply.github.com>
2026-09-15 08:55:55 +0000

    chore(homebrew): update formula to v1.0.1

--- Formula/sumanmovies-tui.rb
+++ Formula/sumanmovies-tui.rb
@@ -1,8 +1,8 @@
 class SumanmoviesTui < Formula
   VERSION = "1.0.1"
-  MACOS_SHA256 = "438501f9f525f39153300aed532f0ffda37f92327a8cfcc4e3eb749a664ffdca"
-  LINUX_X64_SHA256 = "bf4041f8c08f3b9e48454a0c0a476d89ec5ecf75b5f93f2e8b80894bbcdcf17f"
-  LINUX_ARM64_SHA256 = "bf33686cba95b30bd633e4e2365f195ede932fe38758386c10cba6e3561f2845"
+  MACOS_SHA256 = "92d4181b7c02d1e10d35b602cbc1602257d1fa6b9227df305de123d13d4bfa31"
+  LINUX_X64_SHA256 = "ef136e02df801dacf135039bd08a2ac896bd3380df1a88ea52d85368903a4bd9"
+  LINUX_ARM64_SHA256 = "ce9205dab01b42ffda33f0873e0eb920a9e32e4b24967c285c55f9ca5fd31358"
 
   desc "Stream movies, shows, anime, and live TV from your terminal"
   homepage "https://github.com/SumanCH8514/SumanMovies-Tui"
